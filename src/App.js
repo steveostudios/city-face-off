9599a2135c84aed3ae0d7e527296ac15bb285cdd
Steve Stone <steve@downloadyouthministry.com>
2022-01-22 14:49:58 -0500

    Can select a city now

--- src/App.js
+++ src/App.js
@@ -3,27 +3,31 @@ import './App.css';
 import { useEffect, useState } from "react";
 
 function App() {
-  const [cities,setCities] = useState()
+  const [cities,setCities] = useState();
+  const [cityA, setCityA] = useState("");
+  const [cityB, setCityB] = useState("");
+
 
   useEffect(() => {
     if (!cities) {
-
       fetch('https://api.teleport.org/api/continents/geonames%3ANA/urban_areas')
-      .then(response => response.json())
-      .then(data => {
-        console.log(data)
-        setCities(data._links["ua:items"]);
-      });
-      }
-      })
+        .then(response => response.json())
+        .then(data => {
+          console.log(data)
+          setCities(data._links["ua:items"]);
+        });
+    }
+  })
 
   return (
     <div className="App">
       <header className="App-header">
+        <h1>
         City Face-Off
+        </h1>
       </header>
-      <CityPicker cities={cities}/>
-      <CityPicker cities={cities}/>
+      <CityPicker cities={cities} setCity={setCityA} city={cityA}/>
+      <CityPicker cities={cities} setCity={setCityB} city={cityB} />
     </div>
   );
 }
--- src/CityPicker.js
+++ src/CityPicker.js
@@ -1,12 +1,12 @@
 function CityPicker(props) {
   return (
-    
     <div>
       City Picker
       {props.cities ?
-      <select>
+      <select onChange={e=> e.target.value}>
+        <option>Select a city</option>
         {props.cities.map(city =>
-          <option key={city.name} value="">{city.name}</option>
+          <option key={city.name} value={city.name}>{city.name}</option>
         )}
       </select>
       : "loading"}
